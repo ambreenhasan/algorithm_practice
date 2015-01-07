d14466fc7007c463ef30dd1540a3c0167002b4ca
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-06 20:37:48 -0800

    unique string added

---
+++ unique_string.rb
@@ -0,0 +1,18 @@
+# Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+def unique_characters_in_a_string(string)
+  counts = {}
+
+  string.downcase.each_char do |char|
+    next if char == " "
+    counts[char] = 0 unless counts.include?(char)
+    counts[char] += 1
+  end
+  p counts
+end
+
+string = "ambreen"
+new_string = "hello how are you"
+
+unique_characters_in_a_string string
+unique_characters_in_a_string new_string
