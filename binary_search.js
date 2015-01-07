07b6680957cfd35960d98ef6d6b9521b99f60bb4
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-06 21:45:42 -0800

    binary search

--- binary_search.js
+++ binary_search.js
@@ -1,3 +1,32 @@
-function binarySearch(array) {
+function binarySearch(array, searchElement) {
+  // sort the array
+  sortedArray = array.sort(function(a, b){
+    return a - b;
+  });
 
+  var minIndex = 0;
+  var maxIndex = sortedArray.length - 1;
+  var currentIndex;
+  var currentElement;
+
+  while (minIndex <= maxIndex) {
+    currentIndex = Math.floor((minIndex + maxIndex) / 2);
+    currentElement = sortedArray[currentIndex]
+
+
+    if (currentElement < searchElement) {
+      minIndex = currentIndex + 1;
+    } else if (currentElement > searchElement) {
+      maxIndex = currentIndex - 1;
+    } else {
+      return currentIndex;
+    }
+  }
+  return -1;
 }
+
+unsorted_array = [5, 3, 9, 10, 4, 1];
+unsorted_array_dups = [5, 5, 5, 8, 9, 10, 10, 1, 1];
+binarySearch(unsorted_array, 3)
+// binarySearch(unsorted_array, 1)
+// binarySearch(unsorted_array, 10)
