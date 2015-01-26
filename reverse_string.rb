5f8fcdf65db93a47b6f06e680676398be0eafcf7
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-26 14:15:26 -0800

    added benchmark library

--- reverse_string.rb
+++ reverse_string.rb
@@ -1,10 +1,12 @@
+require 'benchmark'
+
 # Input: "ambreen"
 # Output: "neerbma"
 # create a method that will take one argument
 # iterate over the array and swap the last index with
 # the first index
 
-def reverse(string)
+def reverse_one(string)
   len = string.length
 
   (len / 2).times do |letter|
@@ -14,6 +16,12 @@ def reverse(string)
   return string
 end
 
+def reverse_two(string)
+  puts string.split("").reverse.join("")
+end
 
-p reverse 'ambreen'
-p reverse 'hello how are you'
+puts Benchmark
+
+
+p reverse_one 'ambreen'
+p reverse_two 'hello how are you'
