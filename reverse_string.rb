13ab6df850e320a3e242ea090713eda38583f745
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-25 21:32:32 -0800

    added solution

--- reverse_string.rb
+++ reverse_string.rb
@@ -4,3 +4,16 @@
 # iterate over the array and swap the last index with
 # the first index
 
+def reverse(string)
+  len = string.length
+
+  (len / 2).times do |letter|
+    string[-1 - letter], string[letter] = string[letter] , string[-1 - letter]
+  end
+
+  return string
+end
+
+
+p reverse 'ambreen'
+p reverse 'hello how are you'
