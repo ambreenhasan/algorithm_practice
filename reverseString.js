f81eb029532ffc15d5c00d4231143139f5af94d8
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-26 15:42:51 -0800

    added benchmark tests for longer string

--- reverseString.js
+++ reverseString.js
@@ -24,7 +24,7 @@ suite.add("Reverse String Test One", function(){
   reverseOne("ambreen");
 })
 .add("Reverse String Test Two", function(){
-  reverseTwo("ambreen")
+  reverseTwo("ambreen");
 })
 .on('cycle', function(event){
   console.log(String(event.target))
@@ -33,4 +33,18 @@ suite.add("Reverse String Test One", function(){
   console.log("Fastest is " + this.filter("fastest").pluck("name"));
 })
 .run({async: true});
+
+// suite.add("Reverse String Test: Longer String 1", function(){
+//   reverseOne("hello how are you");
+// })
+// .add("Reverse String Test: Longer String 2", function(){
+//   reverseTwo("hello how are you");
+// })
+// .on('cycle', function(event){
+//   console.log(String(event.target))
+// })
+// .on('complete', function(){
+//   console.log("Fastest is " + this.filter("fastest").pluck("name"));
+// })
+// .run({async: true});
 
