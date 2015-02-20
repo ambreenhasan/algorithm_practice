20f0046a601ed11a5c1fbdf4cc654afd8ccd301a
Ambreen Hasan <ambodumbo@gmail.com>
2015-02-19 16:46:29 -0800

    made a possible skeleton of classes

--- six_degrees_of_kevin_bacon.rb
+++ six_degrees_of_kevin_bacon.rb
@@ -1,5 +1,19 @@
 Node = Struct.new(:value, :next)
 
 Friend = Struct.new(:head) do
+  def degrees_of_separation(start_node, destination_node)
+  end
 
+  def make_mutual_friends(friend_one, friend_two)
+    create_relationships(friend_one, friend_two)
+    # find a node
+  end
+
+  def create_relationships(friend_one, friend_two)
+    # create a node
+  end
+
+  def create_person(name)
+    # add a node
+  end
 end
